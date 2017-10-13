efd9c9e14816a2d927e36a240ef8b5975d98929d
Owain Llewellyn <owain.llew@gmail.com>
2017-10-13 16:34:44 -0700

    Added initial project files

---
+++ README.md
@@ -0,0 +1,10 @@
+# Static Site Foundation
+
+Using a combination of AWS Lambda, Contentful and S3 to generate static websites.
+
+## Technologies
+
+* AWS Lambda
+* AWS S3
+* Contentful
+* Handlebars
---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	contentful: {
+		name: "company_website",
+		spaceId: "jciqgcuub56q",
+		accessToken: "6cd7195a2f3bc7d3bc0ed0391406c158b911b3a3ab74aea47b6fb15a14499adf",
+		accessTokenPreview: "afe2fdeed0d102973df13aeb579c03fc1c89da46535c5d49b8b415729640d933"
+	}
+}
