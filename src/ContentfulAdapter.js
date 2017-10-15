147989936db9544a43a8d9120b3469f38f9d1b60
System Administrator <root@Owains-MacBook-Pro.local>
2017-10-14 20:38:21 -0700

    Added Contentful Adapter

--- src/ContentfulAdapter.js
+++ src/ContentfulAdapter.js
@@ -1,7 +1,34 @@
-module.exports = (() => {
-	console.log(process.argv[2]);
+const Contentful = require('contentful');
+const Config = require('../config').contentful;
+
+class ContentfulAdapter {
+	constructor(Config) {
+		this.client = Contentful.createClient({
+			space: Config.spaceId,
+			accessToken: Config.accessToken
+		});
+
+		this.syncContent().then(data => {
+			console.log('data', data.nextSyncToken);
+		}).catch(err => {
+			console.log('error', err);
+		})
+	}
+
+	syncContent() {
+		return this.client.sync({initial: true});
+	}
+
+	getContent() {
+		return this.client.getEntries();
+	}
+
+	getContentById(id) {
+		return this.client.getEntries({'sys.id': id});
+	}
 
 
+}
 
+let ContentfulInstance = new ContentfulAdapter(Config);
 
-})();
